b4fa9dc4ea952757b53a15fe023a7de248e11172
Lewis Hemens <lewis@hemens.co.uk>
2014-01-05 00:50:43 +0000

    Fixed compilation scripts and small type error

--- js-lib/launch.js
+++ js-lib/launch.js
@@ -7,7 +7,7 @@ goog.require('asciiflow.View');
 
 asciiflow.launch = function() {
     var view = new asciiflow.View();
-    var context = view.getCsontext();
+    var context = view.getContext();
     context.font = 'italic 10pt Calibri';
     context.fillText('Hello World!', 150, 100);
 };
