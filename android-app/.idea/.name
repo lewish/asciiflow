c36ef787d3bf7ae9e869b8bc8f432b12cd92ede9
Lewis Hemens <lewis@hemens.co.uk>
2014-02-21 00:10:44 +0000

    Initial base version of the android app wrapper, built with android studio

---
+++ android-app/.idea/.name
@@ -0,0 +1,1 @@
+ASCIIFlow Infinity
---
+++ android-app/asciiflow-infinity/src/main/java/com/lewish/asciiflow/AsciiflowApp.java
@@ -0,0 +1,47 @@
+package com.lewish.asciiflow;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class AsciiflowApp extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        webView = (WebView) findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://lewish.github.io/asciiflow2/app");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.web_view, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
