6a1a572f50c0fb1a976db6aed9203ece7572350b
Brian Schlenker <bschlenk@umich.edu>
2017-01-15 23:20:14 -0800

    Update jquer and closure compiler

--- compile.sh
+++ compile.sh
@@ -8,6 +8,6 @@ java -client -jar closure-compiler.jar \
   --js js-lib/drive-controller.js \
   --js js-lib/input-controller.js \
   --js js-lib/launch.js \
-  --warning_level=VERBOSE --formatting=PRETTY_PRINT --language_in=ECMASCRIPT5 --compilation_level=ADVANCED_OPTIMIZATIONS \
-  --externs=jquery-1.9-externs.js \
+  --warning_level=VERBOSE --formatting=PRETTY_PRINT --language_in=ECMASCRIPT6 --compilation_level=ADVANCED_OPTIMIZATIONS \
+  --externs=jquery-3.1-externs.js \
   > js-compiled.js
