141b8eff9270156883f70ab2e20179122b308f93
Brian Schlenker <bschlenk@umich.edu>
2017-01-21 06:55:18 -0800

    Updated project to use es6 classes, arrow functions, latest closure-compiler, latest jquery (#77)
    * Update jquer and closure compiler
    
    * Move to es6 classes, arrow functions, for/of
    
    * Move to es6 style modules
    
    * Add .DS_Store to .gitignore
    
    * Remove note to update compile.sh

--- js-lib/common.js
+++ js-lib/common.js
@@ -2,241 +2,140 @@
  * Common classes and constants.
  */
 
-// Define namespace for closure compiler but don't make it a requirement.
-try {
-  goog.provide('ascii');
-  throw 1;
-} catch (e) {
-  /** type {Object} */
-  window.ascii = window.ascii || {};
-}
-
-/** @const */ var MAX_GRID_WIDTH = 2000;
-/** @const */ var MAX_GRID_HEIGHT = 600;
-
-/** @const */ var SPECIAL_VALUE = '+';
-/** @const */ var ALT_SPECIAL_VALUE = '^';
-/** @const */ var SPECIAL_ARROW_LEFT = '<';
-/** @const */ var SPECIAL_ARROW_UP = '^';
-/** @const */ var SPECIAL_ARROW_RIGHT = '>';
-/** @const */ var SPECIAL_ARROW_DOWN = 'v';
-/** @const */ var SPECIAL_VALUES = ['+', '\u2012', '\u2013', '-', '|'];
-/** @const */ var ALT_SPECIAL_VALUES = ['>', '<', '^', 'v'];
-/** @const */ var ALL_SPECIAL_VALUES = SPECIAL_VALUES.concat(ALT_SPECIAL_VALUES);
-
-/** @const */ var MAX_UNDO = 50;
-
-/** @const */ var SPECIAL_LINE_H = '-';
-/** @const */ var SPECIAL_LINE_V = '|';
-
-/** @const */ var ERASE_CHAR = '\u2009';
-
-/** @const */ var DRAG_LATENCY = 150; // Milliseconds.
-/** @const */ var DRAG_ACCURACY = 6; // Pixels.
-
-/** @const */ var CHAR_PIXELS_H = 9;
-/** @const */ var CHAR_PIXELS_V = 17;
-
-/** @const */ var RENDER_PADDING_CELLS = 3;
-
-/** @const */ var KEY_RETURN = '<enter>';
-/** @const */ var KEY_BACKSPACE = '<backspace>';
-/** @const */ var KEY_COPY = '<copy>';
-/** @const */ var KEY_PASTE = '<paste>';
-/** @const */ var KEY_CUT = '<cut>';
-/** @const */ var KEY_UP = '<up>';
-/** @const */ var KEY_DOWN = '<down>';
-/** @const */ var KEY_LEFT = '<left>';
-/** @const */ var KEY_RIGHT = '<right>';
-
-// http://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript
-/** @const */ var TOUCH_ENABLED =
-    'ontouchstart' in window ||
-    'onmsgesturechange' in window;
-
-/**
- * Stores a 2D vector.
- *
- * @constructor
- * @param {number} x
- * @param {number} y
- */
-ascii.Vector = function(x, y) {
-  /** type {Number} */ this.x = x;
-  /** type {Number} */ this.y = y;
-};
-
-/**
- * @param {ascii.Vector} other
- * @return {boolean}
- */
-ascii.Vector.prototype.equals = function(other) {
-  return (other != null) && (this.x == other.x) && (this.y == other.y);
-};
-
-/**
- * @param {ascii.Vector} other
- * @return {ascii.Vector}
- */
-ascii.Vector.prototype.subtract = function(other) {
-  return new ascii.Vector(this.x - other.x, this.y - other.y);
-};
-
-/**
- * @param {ascii.Vector} other
- * @return {ascii.Vector}
- */
-ascii.Vector.prototype.add = function(other) {
-  return new ascii.Vector(this.x + other.x, this.y + other.y);
-};
-
-/**
- * @return {ascii.Vector}
- */
-ascii.Vector.prototype.clone = function() {
-  return new ascii.Vector(this.x, this.y);
-};
-
-/** @return {number} */
-ascii.Vector.prototype.length = function() {
-  return Math.sqrt(this.x * this.x + this.y * this.y);
-};
-
-/**
- * @param {number} scale
- * @return {ascii.Vector}
- */
-ascii.Vector.prototype.scale = function(scale) {
-  return new ascii.Vector(this.x * scale, this.y * scale);
-};
+import { ERASE_CHAR, ALL_SPECIAL_VALUES } from './constants';
+import Vector from './vector';
 
 /**
  * Represents a box with normalized position vectors.
- *
- * @constructor
- * @param {ascii.Vector} a
- * @param {ascii.Vector} b
  */
-ascii.Box = function(a, b) {
-  /** type {Number} */ this.startX = Math.min(a.x, b.x);
-  /** type {Number} */ this.startY = Math.min(a.y, b.y);
-  /** type {Number} */ this.endX = Math.max(a.x, b.x);
-  /** type {Number} */ this.endY = Math.max(a.y, b.y);
-};
+export class Box {
+  /**
+   * @param {Vector} a
+   * @param {Vector} b
+   */
+  constructor(a, b) {
+    /** type {number} */ this.startX = Math.min(a.x, b.x);
+    /** type {number} */ this.startY = Math.min(a.y, b.y);
+    /** type {number} */ this.endX = Math.max(a.x, b.x);
+    /** type {number} */ this.endY = Math.max(a.y, b.y);
+  }
 
-/** @return {ascii.Vector} */
-ascii.Box.prototype.topLeft = function() {
-  return new ascii.Vector(this.startX, this.startY);
-};
+  /** @return {Vector} */
+  topLeft() {
+    return new Vector(this.startX, this.startY);
+  }
 
-/** @return {ascii.Vector} */
-ascii.Box.prototype.bottomRight = function() {
-  return new ascii.Vector(this.endX, this.endY);
-};
+  /** @return {Vector} */
+  bottomRight() {
+    return new Vector(this.endX, this.endY);
+  }
 
-/** @return {boolean} */
-ascii.Box.prototype.contains = function(position) {
-  return position.x >= this.startX && position.x <= this.endX && position.y >= this.startY && position.y <= this.endY;
-};
-
-/** @const */ var DIR_LEFT = new ascii.Vector(-1, 0);
-/** @const */ var DIR_RIGHT = new ascii.Vector(1, 0);
-/** @const */ var DIR_UP = new ascii.Vector(0, -1);
-/** @const */ var DIR_DOWN = new ascii.Vector(0, 1);
-
-/** @const */ var DIRECTIONS = [DIR_LEFT, DIR_RIGHT, DIR_UP, DIR_DOWN];
+  /** @return {boolean} */
+  contains(position) {
+    return position.x >= this.startX && position.x <= this.endX
+        && position.y >= this.startY && position.y <= this.endY;
+  }
+}
 
 /**
  * An individual cell within the diagram and it's current value.
- *
- * @constructor
  */
-ascii.Cell = function() {
-  /** @type {?string} */ this.value = null;
-  /** @type {?string} */ this.scratchValue = null;
-};
+export class Cell {
 
-/** @return {?string} */
-ascii.Cell.prototype.getRawValue = function() {
-  return (this.scratchValue != null ? this.scratchValue : this.value);
-};
+  constructor() {
+    /** @type {?string} */ this.value = null;
+    /** @type {?string} */ this.scratchValue = null;
+  }
 
-/** @return {boolean} */
-ascii.Cell.prototype.isSpecial = function() {
-  return ALL_SPECIAL_VALUES.indexOf(this.getRawValue()) != -1;
-};
+  /** @return {?string} */
+  getRawValue() {
+    return (this.scratchValue != null ? this.scratchValue : this.value);
+  }
 
-/** @return {boolean} */
-ascii.Cell.prototype.isEmpty = function() {
-  return this.value == null && this.scratchValue == null;
-};
+  /** @return {boolean} */
+  isSpecial() {
+    return ALL_SPECIAL_VALUES.indexOf(this.getRawValue()) != -1;
+  }
 
-/** @return {boolean} */
-ascii.Cell.prototype.hasScratch = function() {
-  return this.scratchValue != null;
-};
+  /** @return {boolean} */
+  isEmpty() {
+    return this.value == null && this.scratchValue == null;
+  }
 
-/** @return {boolean} */
-ascii.Cell.prototype.isErase = function() {
-  return this.scratchValue == ERASE_CHAR;
-};
+  /** @return {boolean} */
+  hasScratch() {
+    return this.scratchValue != null;
+  }
+
+  /** @return {boolean} */
+  isErase() {
+    return this.scratchValue == ERASE_CHAR;
+  }
+}
 
 /**
  * The context for a cell, i.e. the status of the cells around it.
- *
- * @param {boolean} left
- * @param {boolean} right
- * @param {boolean} up
- * @param {boolean} down
- * @constructor
  */
-ascii.CellContext = function(left, right, up, down) {
-  /** @type {boolean} */ this.left = left;
-  /** @type {boolean} */ this.right = right;
-  /** @type {boolean} */ this.up = up;
-  /** @type {boolean} */ this.down = down;
-  /** @type {boolean} */ this.leftup = false;
-  /** @type {boolean} */ this.rightup = false;
-  /** @type {boolean} */ this.leftdown = false;
-  /** @type {boolean} */ this.rightdown = false;
-};
+export class CellContext {
+  /**
+   * @param {boolean} left
+   * @param {boolean} right
+   * @param {boolean} up
+   * @param {boolean} down
+   */
+  constructor(left, right, up, down) {
+    /** @type {boolean} */ this.left = left;
+    /** @type {boolean} */ this.right = right;
+    /** @type {boolean} */ this.up = up;
+    /** @type {boolean} */ this.down = down;
+    /** @type {boolean} */ this.leftup = false;
+    /** @type {boolean} */ this.rightup = false;
+    /** @type {boolean} */ this.leftdown = false;
+    /** @type {boolean} */ this.rightdown = false;
+  }
 
-/**
- * Returns the total number of surrounding special cells.
- * @return {number}
- */
-ascii.CellContext.prototype.sum = function() {
-  return this.left + this.right + this.up + this.down;
-};
+  /**
+   * Returns the total number of surrounding special cells.
+   * @return {number}
+   */
+  sum() {
+    return this.left + this.right + this.up + this.down;
+  }
 
-/**
- * Returns the total number of surrounding special cells.
- * @return {number}
- */
-ascii.CellContext.prototype.extendedSum = function() {
-  return this.left + this.right + this.up + this.down + this.leftup + this.leftdown + this.rightup + this.rightdown;
-};
+  /**
+   * Returns the total number of surrounding special cells.
+   * @return {number}
+   */
+  extendedSum() {
+    return this.left + this.right + this.up + this.down
+         + this.leftup + this.leftdown + this.rightup + this.rightdown;
+  }
+}
 
 /**
  * A pair of a vector and a string value. Used in history management.
- * @constructor
- * @struct
- * @param {ascii.Vector} position
- * @param {string|null} value
  */
-ascii.MappedValue = function(position, value) {
-  this.position = position;
-  this.value = value;
-};
+export class MappedValue {
+  /**
+   * @param {Vector} position
+   * @param {string|null} value
+   */
+  constructor(position, value) {
+    this.position = position;
+    this.value = value;
+  }
+}
 
 /**
  * A pair of a vector and a cell. Used in history management.
- * @constructor
- * @struct
- * @param {ascii.Vector} position
- * @param {ascii.Cell} cell
  */
-ascii.MappedCell = function(position, cell) {
-  this.position = position;
-  this.cell = cell;
-};
+export class MappedCell {
+  /**
+   * @param {Vector} position
+   * @param {Cell} cell
+   */
+  constructor(position, cell) {
+    this.position = position;
+    this.cell = cell;
+  }
+}
---
+++ js-lib/constants.js
@@ -0,0 +1,51 @@
+import Vector from './vector';
+
+export const MAX_GRID_WIDTH = 2000;
+export const MAX_GRID_HEIGHT = 600;
+
+export const SPECIAL_VALUE = '+';
+export const ALT_SPECIAL_VALUE = '^';
+export const SPECIAL_ARROW_LEFT = '<';
+export const SPECIAL_ARROW_UP = '^';
+export const SPECIAL_ARROW_RIGHT = '>';
+export const SPECIAL_ARROW_DOWN = 'v';
+export const SPECIAL_VALUES = ['+', '\u2012', '\u2013', '-', '|'];
+export const ALT_SPECIAL_VALUES = ['>', '<', '^', 'v'];
+export const ALL_SPECIAL_VALUES = SPECIAL_VALUES.concat(ALT_SPECIAL_VALUES);
+
+export const MAX_UNDO = 50;
+
+export const SPECIAL_LINE_H = '-';
+export const SPECIAL_LINE_V = '|';
+
+export const ERASE_CHAR = '\u2009';
+
+export const DRAG_LATENCY = 150; // Milliseconds.
+export const DRAG_ACCURACY = 6; // Pixels.
+
+export const CHAR_PIXELS_H = 9;
+export const CHAR_PIXELS_V = 17;
+
+export const RENDER_PADDING_CELLS = 3;
+
+export const KEY_RETURN = '<enter>';
+export const KEY_BACKSPACE = '<backspace>';
+export const KEY_COPY = '<copy>';
+export const KEY_PASTE = '<paste>';
+export const KEY_CUT = '<cut>';
+export const KEY_UP = '<up>';
+export const KEY_DOWN = '<down>';
+export const KEY_LEFT = '<left>';
+export const KEY_RIGHT = '<right>';
+
+// http://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript
+export const TOUCH_ENABLED =
+    'ontouchstart' in window ||
+    'onmsgesturechange' in window;
+
+export const DIR_LEFT  = new Vector(-1,  0);
+export const DIR_RIGHT = new Vector( 1,  0);
+export const DIR_UP    = new Vector( 0, -1);
+export const DIR_DOWN  = new Vector( 0,  1);
+
+export const DIRECTIONS = [DIR_LEFT, DIR_RIGHT, DIR_UP, DIR_DOWN];
