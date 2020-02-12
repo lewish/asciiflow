07c29d653fff6566017d69030930a97c52dad97d
Alex <alex@brewingcode.net>
2020-02-11 16:18:59 -0800

    Only include 3rd party scripts on main url.
    In the case of GA and TPM, developing locally should not pull these in.
    
    In the case of Google Drive, the OAuth app is not setup to allow
    localhost:8000 as an authorization endpoint, so popping up an auth
    screen will alway be an error.

---
+++ third-party.js
@@ -0,0 +1,22 @@
+<script src="https://apis.google.com/js/client.js?onload=window.gapiCallback"></script>
+
+<script type="text/javascript">
+var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+if (width > 750) {
+  window._tpm = window._tpm || [];
+  window._tpm['paywallID'] = '37774341';
+  window._tpm['trackPageview'] = true;
+}
+</script>
+<script type="text/javascript" src="https://code.tinypass.com/tpl/d1/tpm.js"></script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','analytics');
+
+  analytics('create', 'UA-49427709-1', 'asciiflow.com');
+  analytics('send', 'pageview');
+
+</script>
